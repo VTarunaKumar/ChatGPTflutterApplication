4c74c706a31c5893fb525e21c98500d94a79f707
VTarunaKumar <vtarunakumarrao@gmail.com>
2023-01-31 15:07:37 +0530

    Completed but API key have to buy

--- lib/chat_message.dart
+++ lib/chat_message.dart
@@ -9,24 +9,11 @@ class ChatMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Container(
-          margin: const EdgeInsets.only(right: 16.0),
-          child: CircleAvatar(child: Text(sender[0]),),
-        ),
-        Expanded(child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [Text(sender, style: Theme
-              .of(context)
-              .textTheme
-              .subtitle1,),
-            Container(
-              margin: const EdgeInsets.only(top: 5.0),
-              child: Text(text),
-            )
-          ],
-        ))
-      ],
-    );
+        Text(sender).text.subtitle1(context).make().box.color(sender=="user" ? Vx.red200 : Vx.green200).p16.rounded.alignCenter.makeCentered(),
+        Expanded(child: text.trim().text.bodyText1(context).make().px8(),
+        )],
+    ).py8();
   }
 }
--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:chatgpt_ai_app_flutter/chat_message.dart';
+import 'package:chatgpt_ai_app_flutter/three_dots.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 class ChatScreen extends StatefulWidget {
@@ -11,13 +15,50 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _controller = TextEditingController();
   final List<ChatMessage> _messages = [];
+
+
+ChatGPT? chatGPT;
+StreamSubscription? _subscription;
+bool _isTyping  =  false;
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    chatGPT = ChatGPT.instance;
+  }
+@override
+  void dispose() {
+   _subscription?.cancel();
+    super.dispose();
+  }
+
   void _sendMessage(){
     ChatMessage message = ChatMessage(text: _controller.text, sender: "user");
     setState(() {
       _messages.insert(0, message);
+      _isTyping = true;
     });
     _controller.clear();
+   final request =  CompleteReq(prompt: message.text, model : kTranslateModelV3, max_tokens : 200 );
+
+    _subscription = chatGPT!
+        .builder("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",orgId: "")
+        .onCompleteStream(request: request)
+        .listen((response) {
+      Vx.log(response!.choices[0].text);
+      ChatMessage botMessage  = ChatMessage(text: response!.choices[0].text, sender: "bot",);
+      setState(() {
+        _isTyping = false;
+        _messages.insert(0, botMessage);
+      });
+
+    });
+
+
   }
+
+
 
 
   Widget _buildTextComposer(){
@@ -56,6 +97,10 @@ class _ChatScreenState extends State<ChatScreen> {
             return _messages[index];
 
             },),),
+            if(_isTyping) ThreeDots(),
+            Divider(
+              height: 1.0,
+            ),
             Container(
               decoration: BoxDecoration(
                 color: context.cardColor,
